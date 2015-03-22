60cc1539a345b46341b4b919f457167b04d2fa88
azaz13 <elsa.martel@free.fr>
2015-03-22 10:37:03 +0100

    Vérification lorsqu'on veut rejoindre un groupe
    avec une requete select car ca ne marche pas avec l'autre méthode

--- app/Controller/GroupesController.php
+++ app/Controller/GroupesController.php
@@ -52,27 +52,63 @@
             if (!$groupe) {
                 throw new NotFoundException(__('Invalid groupe'));
             }
+
             $val = AuthComponent::user('Membre');
             $membre = $this->Groupe->Membre->findByUsername($val['username']);
 
             $this->set('groupe', $groupe);
             $this->set('membre', $membre);
 
+            /*memoriser l'id de l'utilisateur et l'id du groupe*/
             $d['membre_id']=$membre['Membre']['id'];
             $d['groupe_id']=$groupe['Groupe']['id'];
 
             $this->loadModel('MembresGroupes');
             $this->MembresGroupes->create();
-            /*faire des modifications */
-            
-            if($this->MembresGroupes->save($d, true, array('membre_id','groupe_id')))
+
+            /*recherche dans la table membres_groupes*/
+            /*$options['joins']=array(
+            array(
+                'table' => 'groupes',
+                'alias' => 'G',
+                'conditions' => array('MembresGroupes.groupe_id = G.id')
+                ),
+            array(
+                'table' => 'membres',
+                'alias' => 'M',
+                'conditions' => array('MembresGroupes.membre_id = M.id')
+                )
+            );
+            $options['conditions'] = array(
+            'MembresGroupes.membre_id' => $d['membre_id'],
+            'MembresGroupes.groupe_id' => $d['groupe_id']
+            );
+           $options['fields'] = array(
+            'MembresGroupes.membre_id', 'MembresGroupes.groupe_id'
+            );
+            $mg = $this->MembresGroupes->find('all',$options);*/
+            $mg = $this->MembresGroupes->query("
+                SELECT *
+                FROM membres_groupes
+                WHERE membre_id = {$d['membre_id']} AND groupe_id = {$d['groupe_id']};"
+            );
+            debug($mg);
+            if(empty($mg))
             {
-                $this->Session->setFlash("Vous avez rejoint le groupe", "notif");
+                if($this->MembresGroupes->save($d, true, array('membre_id','groupe_id')))
+                {
+                    $this->Session->setFlash("Vous avez rejoint le groupe", "notif");
+                }
+                else
+                {
+                    $this->Session->setFlash("Merci de corriger vos erreurs", "notif");
+                }
             }
             else
             {
-                $this->Session->setFlash("Merci de corriger vos erreurs", "notif");
+                $this->Session->setFlash("Vous êtes déjà membre", "notif");
             }
+            
 
 
             /*$options['joins']=array(
