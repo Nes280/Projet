718de6ff92f9f248995b80eb065d69cea83c237f
azaz13 <elsa.martel@free.fr>
2015-04-17 15:29:14 +0200

    formulaire d'ajout de film

--- app/Controller/FormatsController.php
+++ app/Controller/FormatsController.php
@@ -47,6 +47,58 @@
         $this->set('filmsFormat',$lesFilms);
 
     }
+
+
+        public function ajoutformat($id=null){
+        if (!$id) {
+            throw new NotFoundException(__('Film non trouvé'));
+        }
+        $idfilm = $id;
+        $film = $this->Format->Film->findById($idfilm);
+        $this->set('film', $film);
+        $d = $this->request->data;
+
+        if($this->request->is('post'))
+        {
+            $d['Format']['id'] = null;
+            $valeur = 0;
+            $resultat = $this->Format->query("SELECT id FROM formats WHERE format='{$d['Format']['format']}';");
+
+            if(empty($resultat))
+            {
+                if($this->Format->save($d, true, array('format')))
+                {
+                    $valeur = 1;
+                }
+                else
+                {
+                    $this->Session->setFlash("Merci de corriger vos erreurs", "notif");
+                }
+            }
+            else
+            {
+                $valeur =1;
+            }
+
+            if($valeur == 1)
+            {
+                $res = $this->Format->query("SELECT * FROM formats WHERE format='{$d['Format']['format']}';");
+                $val['film_id']=$idfilm;
+                $val['format_id'] = $res['0']['formats']['id'];
+                $this->loadModel('FilmsFormats');
+                $this->FilmsFormats->create();
+                if($this->FilmsFormats->save($val, true, array('film_id', 'format_id')))
+                {
+                    $this->redirect(array('controller' => 'Formats', 'action' => 'ajoutformat', $id));
+                }
+                else
+                {
+                    $this->Session->setFlash("Merci de corriger vos erreurs", "notif");
+
+                }
+            }
+        }
+    }
 }
 
 ?>
---
+++ app/View/Formats/ajoutformat.ctp
@@ -0,0 +1,23 @@
+<div class="row">
+	<h2>Ajout du format</h2>
+</div>
+<?php echo $this->Form->create('Format'); ?>
+	<div class="row">
+	    <div class="large-4 columns">
+			<?php echo $this->Form->input('format',array('label' =>"Format", "type"=>"text", "maxlength"=>"30")); ?>
+		</div>
+	</div>
+	<div class="row">
+		<?php echo $this->Form->end('Enregistrer'); ?>
+	</div>
+
+	<?php echo $this->Html->link("Ajout d'acteur", array('controller' => 'Acteurs','action'=>'ajoutacteur', $film['Film']['id']), array('class' => 'button success')); ?>
+
+	<?php echo $this->Html->link('Ajout du distributeur', array('controller' => 'Distributeurs','action'=>'ajoutdistributeur', $film['Film']['id']), array('class' => 'button success')); ?>
+
+	<?php echo $this->Html->link('Ajout du genre', array('controller' => 'Genres','action'=>'ajoutgenre', $film['Film']['id']), array('class' => 'button success')); ?>
+
+	<?php echo $this->Html->link('Ajout du pays', array('controller' => 'Pays','action'=>'ajoutpays', $film['Film']['id']), array('class' => 'button success')); ?>
+
+	<?php echo $this->Html->link('Ajout du réalisateur', array('controller' => 'Realisateurs','action'=>'ajoutrealisateur', $film['Film']['id']), array('class' => 'button success')); ?>
+	<?php echo $this->Html->link('Ajout du producteur', array('controller' => 'Producteurs','action'=>'ajoutproducteur', $film['Film']['id']), array('class' => 'button success')); ?>
